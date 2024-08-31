d76ceaf6cf5cdb2363e1067ba2591776305129c1
Facundo Poblete <108022463+ddl-fpoblete@users.noreply.github.com>
2024-08-31 19:53:26 -0300

    #15475 changes

--- lib/kube/proxy/remotecommand.go
+++ lib/kube/proxy/remotecommand.go
@@ -32,6 +32,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/httpstream"
 	spdystream "k8s.io/apimachinery/pkg/util/httpstream/spdy"
 	remotecommandconsts "k8s.io/apimachinery/pkg/util/remotecommand"
+	"k8s.io/apiserver/pkg/util/wsstream"
 	"k8s.io/client-go/tools/remotecommand"
 	utilexec "k8s.io/client-go/util/exec"
 )
@@ -92,6 +93,23 @@ func (req remoteCommandRequest) eventPodMeta(ctx context.Context, creds *kubeCre
 }
 
 func createRemoteCommandProxy(req remoteCommandRequest) (*remoteCommandProxy, error) {
+	var (
+		proxy *remoteCommandProxy
+		err   error
+	)
+	if wsstream.IsWebSocketRequest(req.httpRequest) {
+		proxy, err = createWebSocketStreams(req)
+	} else {
+		proxy, err = createSPDYStreams(req)
+	}
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	return proxy, nil
+}
+
+func createSPDYStreams(req remoteCommandRequest) (*remoteCommandProxy, error) {
 	protocol, err := httpstream.Handshake(req.httpRequest, req.httpResponseWriter, []string{StreamProtocolV4Name})
 	if err != nil {
 		return nil, trace.Wrap(err)
@@ -365,6 +383,16 @@ func v4WriteStatusFunc(stream io.Writer) func(status *apierrors.StatusError) err
 			return err
 		}
 		_, err = stream.Write(bs)
+		return err
+	}
+}
+
+func v1WriteStatusFunc(stream io.Writer) func(status *apierrors.StatusError) error {
+	return func(status *apierrors.StatusError) error {
+		if status.Status().Status == metav1.StatusSuccess {
+			return nil // send error messages
+		}
+		_, err := stream.Write([]byte(status.Error()))
 		return err
 	}
 }
